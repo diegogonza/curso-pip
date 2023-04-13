f6672a4ac0c00ff1c81051b71caca8a1ecc6b424
diegogonza <diegoandresgonzalezdiaz@gmail.com>
2023-04-13 15:19:43 -0500

    create a web server with fastapi and requests

---
+++ web-server/main.py
@@ -0,0 +1,21 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,3,4]
+
+@app.get('/contacto', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola FastAPI</h1>
+    """        
+
+def run():
+    store.get_categories()
+
+
+if __name__ == '__main__':
+    run()
